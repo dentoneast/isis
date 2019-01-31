6e034bd53829cb4f598a1770d375610e064751e0
Andi Huber <ahuber@apache.org>
2019-01-31 12:45:38 +0100

    ISIS-2033: simple-app: new menu action: 'create some customers'
    Task-Url: https://issues.apache.org/jira/browse/ISIS-2033

--- example/application/simpleapp/module-spring/src/main/java/domainapp/modules/spring/dom/customer/CustomerMenu.java
+++ example/application/simpleapp/module-spring/src/main/java/domainapp/modules/spring/dom/customer/CustomerMenu.java
@@ -52,4 +52,22 @@ public class CustomerMenu {
 		return customerRepository.save(new Customer(firstName, lastName));
 	}
 	
+	@Action
+	@ActionLayout(cssClassFa="fa-leaf")
+	public List<Customer> createSomeCustomers() {
+
+		// save a couple of customers
+		customerRepository.save(new Customer("Jack", "Bauer"));
+		customerRepository.save(new Customer("Chloe", "O'Brian"));
+		customerRepository.save(new Customer("Kim", "Bauer"));
+		customerRepository.save(new Customer("David", "Palmer"));
+		customerRepository.save(new Customer("Michelle", "Dessler"));
+		
+		return listAll();
+		
+	}
+	
+
+
+	
 }
